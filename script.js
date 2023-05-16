1cee134b42b9c35e99c10e5570d5626cf72cf59f
Aleksandr Smelov <smelovalex14@gmail.com>
2023-05-16 19:28:44 +0300

    functionality-done, pre-review refactoring needed

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let editButton = document.querySelector('.button_type_edit');
-let saveButton = document.querySelector('.popup__submit-button');
-let cancelButton = document.querySelector('.popup__cancel-button');
+let saveButton = document.querySelector('.button_type_submit');
+let cancelButton = document.querySelector('.button_type_cancel');
 let likeButtons = document.querySelector('.like-button');
 let popup = document.querySelector('.popup');
 let grid = document.querySelector('.photo-grid');
 
 function openPopup() {
-  popup.classList.remove('disabled');
+  popup.classList.add('popup_opened');
 
   let name = document.querySelector('.profile__username');
   let job = document.querySelector('.profile__user-job');
-  let formName = document.querySelector('.input_type_name');
-  let formJob = document.querySelector('.input_type_job');
+  let formName = document.querySelector('.popup__input_type_name');
+  let formJob = document.querySelector('.popup__input_type_job');
 
   formName.value = name.innerText;
   formJob.value = job.innerText;
@@ -19,11 +19,11 @@ function openPopup() {
   function Submit() {
     name.innerText = formName.value;
     job.innerText = formJob.value;
-    popup.classList.add('disabled');
+    popup.classList.remove('popup_opened');
   }
 
   function Cancel() {
-    popup.classList.add('disabled');
+    popup.classList.remove('popup_opened');
   }
   saveButton.addEventListener('click', Submit);
   cancelButton.addEventListener('click', Cancel);
@@ -32,14 +32,12 @@ function openPopup() {
 grid.addEventListener('click', function (event) {
   let clicked_element = event.target;
   if (clicked_element.classList.contains('like-button')) {
-    console.log('like');
-
-    if (clicked_element.classList.contains('active')) {
-      clicked_element.classList.remove('active');
-      clicked_element.setAttribute('src', 'pictures/like_disabled.svg');
+    if (clicked_element.classList.contains('button_condition_active')) {
+      clicked_element.classList.remove('button_condition_active');
+      clicked_element.setAttribute('src', 'images/like_disabled.svg');
     } else {
-      clicked_element.classList.add('active');
-      clicked_element.setAttribute('src', 'pictures/like_filled.svg');
+      clicked_element.classList.add('button_condition_active');
+      clicked_element.setAttribute('src', 'images/like_filled.svg');
     }
   }
 });
