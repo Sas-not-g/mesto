0be91cd57ca3249bdea68cf775e25b3dcbeaf89a
Aleksandr Smelov <smelovalex14@gmail.com>
2023-05-29 00:15:58 +0300

    new:like-button functionality

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,7 @@ let username = document.querySelector('.profile__username');
 let job = document.querySelector('.profile__job');
 let formName = document.querySelector('.popup__input_type_name');
 let formJob = document.querySelector('.popup__input_type_job');
+let grid = document.querySelector('.photo-grid');
 
 function openPopup() {
   popup.classList.add('popup_opened');
@@ -23,6 +24,19 @@ function submit(evt) {
 function cancel() {
   popup.classList.remove('popup_opened');
 }
+
+function like(event) {
+  let clicked_element = event.target;
+  if (clicked_element.classList.contains('photo-grid__like-button')) {
+    if (clicked_element.classList.contains('photo-grid__like-button_active')) {
+      clicked_element.classList.remove('photo-grid__like-button_active');
+    } else {
+      clicked_element.classList.add('photo-grid__like-button_active');
+    }
+  }
+}
+
 saveButton.addEventListener('submit', submit);
 cancelButton.addEventListener('click', cancel);
 editButton.addEventListener('click', openPopup);
+grid.addEventListener('click', like);
